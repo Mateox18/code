9670e2315e12ada345facae893d7dedc0e23ae9b
Mateox18 <Mateopardox18@gmail.com>
2023-02-19 15:43:16 -0500

    19 de febrero
    sortear datos

--- movimiento_circular_uniforme.cpp
+++ movimiento_circular_uniforme.cpp
@@ -6,7 +6,7 @@
 #include<math.h>
 using namespace std;
 int main(){
-float velT,aceT,T,D,radio,ang,rapA,per,Dt,x0,x;
+float velT,aceT,T,D,radio,ang,rapA,per,Dt,x0,x,xf;
 float rad = 57.29;
     cout<<"Iniciando movimiento_circular_uniforme.exe..."<<endl;
     //Comprobamos cuales son los datos faltantes en el problema
@@ -29,17 +29,20 @@ float rad = 57.29;
     cin>>per;
       cout<<"Falta la posicion inicial?"<<endl;
     cin>>x0;
-       cout<<"Le solicitaron una posicione especifica?"<<endl;
+       cout<<"Le solicitaron una posicion especifica?"<<endl;
     cin>>x;
-    if (velT && aceT && T && radio && ang && rapA && per && Dt && x && x0 <= 1)  
+        cout<<"Le solicitaron la posicion final?"<<endl;
+    cin>>xf;
+    cout<<"Este programa usa como ejemplo problemas de 3 incognitas por lo tanto puede que le de datos que ya tiene"<<endl;
+    if (velT && aceT && T && ang && rapA && per && Dt && x && x0 && xf <= 1)  
     // Verificamos que se hayan introducido los datos de manera correcta con el primer if
     {
-        if(velT && aceT && T && radio && ang && rapA && per && Dt && x && x0 == 1){
+        if(velT && aceT && T && ang && rapA && per && Dt && x && x0 && xf == 1){
            cout<<"Error: Ya conoce todos los datos por lo tanto el problema ya esta resuelto."<<endl;
             }
         }
         else{
-            if(velT && rapA && aceT == 0){ // Si la velocidad y la rapidez angular se desconocen entonces se resuelve de la siguiente manera
+            if(velT && rapA && aceT == 0){ // Si faltan los 3 datos entonces se resuelve de la siguiente manera
                 cout<<"Introduzca el angulo del momento seleccionado: "<<endl;
                 cin>>ang;
                 cout<<"Introduzca el tiempo transcurrido: "<<endl;
@@ -50,22 +53,17 @@ float rad = 57.29;
                 cin>>radio;
                 velT =  rapA * radio;
                 cout<<"La velocidad tangecial es: "<<velT<<endl;
-                aceT = pow(velT, 2) / radio;
+                aceT = pow(velT, 2) / radio; // pow para elevar al cuadrado
                 cout<<"La aceleracion tangencial es: "<<aceT<<endl;
             }else{ 
-            if(rapA && velT == 0){
-                   cout<<"Introduzca el angulo del momento seleccionado: "<<endl;
-                cin>>ang;
-                cout<<"Introduzca el tiempo transcurrido: "<<endl;
-                cin>>T;
-                rapA = ang / T;
-                cout<<"La rapidez angular (velocidad angular si se quiere ver asi) es: "<<rapA<<endl;
-                cout<<"Introduzca el radio del giro solicitado: ";
-                cin>>radio;
-                velT =  rapA * radio;
-                cout<<"La velocidad tangecial es: "<<velT<<endl
+                if (velT && Dt && x && x0 == 0)
+                {
+                    
+                }
+                
 
-            }
+            } 
+            
             }
         }
     }
